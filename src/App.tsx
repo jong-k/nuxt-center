c0d6e86d7408607c7e0432a1673da40bea5d74d2
jhkim-hdxwill <jhkim00@iwillmed.com>
2023-08-02 18:06:48 +0900

    chore: 프로젝트 셋업

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Box, Icon, IconButton } from "@chakra-ui/react";
+import { Square } from "react-feather";
 
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Box
+        position="absolute"
+        top="20px"
+        left="20px"
+        backgroundColor="white"
+        padding={2}
+        boxShadow="md"
+        borderRadius="md"
+      >
+        <IconButton
+          aria-label="Add Rectangle"
+          icon={<Icon style={{ width: 24, height: 24 }} as={Square} />}
+        />
+      </Box>
+    </div>
   );
 }
-
-export default App;
