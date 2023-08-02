f0d3ddeca07e327f850e06d81b879baf375ba639
jhkim-hdxwill <jhkim00@iwillmed.com>
2023-08-02 13:54:18 +0900

    init project

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+// @eslint-ignore
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
