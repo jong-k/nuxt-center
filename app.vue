a94caf32fc211530a567858ec6da26b753b79b7f
jong-k <fully.charged07@gmail.com>
2025-02-05 00:19:44 +0900

    feat: vue 공식 문서 추가

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>haha</div>
+  <p>
+    <!--  <a v-bind:href="url" target="_blank">Vue.js 공식문서</a>  -->
+    <!--  아래는 v-bind directive 축약형  -->
+    <a :href="url" target="_blank">Vue.js 공식문서</a>
+  </p>
+  <p>
+    <a :href="url + 'style-guide'" target="_blank">Vue.js 스타일 가이드</a>
+  </p>
 </template>
 
 <script setup lang="ts">
