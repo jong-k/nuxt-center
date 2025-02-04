4dae7edd4ed245041412e3291f28614b01c143a0
jong-k <fully.charged07@gmail.com>
2025-02-04 16:42:23 +0900

    chore: project change

--- recoil-basic/src/types/index.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-declare interface TodoListItem {
-  id: number;
-  text: string;
-  isDone: boolean;
-}
-
-declare type TodoListFilter = "Show All" | "Show Done" | "Show UnDone";
