69315932f9fd0e34550d1b8306f9e42947d09575
jong-k <fully.charged07@gmail.com>
2025-02-06 07:41:13 +0900

    feat: 여러 v-bind 추가

--- app.vue
+++ app.vue
@@ -1,14 +1,14 @@
 <template>
-  <p>
-    <!--  <a v-bind:href="url" target="_blank">Vue.js 공식문서</a>  -->
-    <!--  아래는 v-bind directive 축약형  -->
-    <a :href="url" target="_blank">Vue.js 공식문서</a>
-  </p>
-  <p>
-    <a :href="url + 'style-guide'" target="_blank">Vue.js 스타일 가이드</a>
-  </p>
+  <div>
+    <img v-bind="imgAttributes" />
+  </div>
 </template>
 
 <script setup lang="ts">
-const url = ref("https://vuejs.org/");
+const imgAttributes = ref({
+  src: "/favicon.ico",
+  alt: "logo",
+  width: 75,
+  height: 75,
+});
 </script>
