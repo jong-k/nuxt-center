db7dd73e83337f824bd87e03ccf60ca77f8d95dd
jong-k <fully.charged07@gmail.com>
2025-02-09 15:56:02 +0900

    feat: 이벤트 버블링 확인

--- app.vue
+++ app.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-    <img v-bind="imgAttributes" alt="nuxt logo" title="nuxt" />
+  <div class="4f" @click="handleClick">
+    <div class="3f" @click="handleClick">
+      <div class="2f" @click="handleClick">
+        <button
+          class="1f"
+          :style="{ padding: '10px', color: 'red' }"
+          @click="handleClick"
+        >
+          클릭
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const imgAttributes = ref({
-  src: "/favicon.ico",
-  alt: "logo",
-  width: 75,
-  height: 75,
-  title: "nuxt logo",
-});
+const handleClick = (e: MouseEvent) => {
+  console.log(e.currentTarget); // 버블링 단계에서 각 부모 요소
+  // console.log(e.target); // event 발생지 근원
+};
 </script>
