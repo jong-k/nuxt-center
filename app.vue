bbb2e12213cc1b04143e422c4676aa4b667975a5
jong-k <fully.charged07@gmail.com>
2025-02-09 22:52:23 +0900

    feat: v-model lazy와 일반 비교

--- app.vue
+++ app.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
-    <input v-model.trim="trimmedText" type="text" />
-    <p>입력 문자열: {{ trimmedText }}</p>
+    <div>
+      <h2>v-model</h2>
+      <input v-model="textOnInput" type="text" />
+      <p>입력 문자열: {{ textOnInput }}</p>
+    </div>
+    <div>
+      <h2>v-model.lazy</h2>
+      <input v-model.lazy="textOnChange" type="text" />
+      <p>입력 문자열: {{ textOnChange }}</p>
+    </div>
+    <div>
+      <h2>v-mode.trim</h2>
+      <input v-model.trim="trimmedText" type="text" />
+      <p>입력 문자열: {{ trimmedText }}</p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+const textOnInput = ref("");
+const textOnChange = ref("");
 const trimmedText = ref("");
 </script>
