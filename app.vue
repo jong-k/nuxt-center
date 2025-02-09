50f27d4249bb011c8210936dd70f1edb2edc8338
jong-k <fully.charged07@gmail.com>
2025-02-09 23:30:47 +0900

    feat: v-html

--- app.vue
+++ app.vue
@@ -1,25 +1,10 @@
 <template>
   <div>
-    <div>
-      <h2>v-model</h2>
-      <input v-model="textOnInput" type="text" />
-      <p>입력 문자열: {{ textOnInput }}</p>
-    </div>
-    <div>
-      <h2>v-model.lazy</h2>
-      <input v-model.lazy="textOnChange" type="text" />
-      <p>입력 문자열: {{ textOnChange }}</p>
-    </div>
-    <div>
-      <h2>v-mode.trim</h2>
-      <input v-model.trim="trimmedText" type="text" />
-      <p>입력 문자열: {{ trimmedText }}</p>
-    </div>
+    <div>{{ htmlStr }}</div>
+    <div v-html="htmlStr"></div>
   </div>
 </template>
 
 <script setup lang="ts">
-const textOnInput = ref("");
-const textOnChange = ref("");
-const trimmedText = ref("");
+const htmlStr = ref(`<a href="https://www.naver.com/">네이버<a/>`);
 </script>
