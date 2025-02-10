24f1976fe9a0dcaccce18a6ade60e3b53ebd7055
jong-k <fully.charged07@gmail.com>
2025-02-10 23:37:13 +0900

    feat: template 태그 조건 분기

--- app.vue
+++ app.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
-    <div>{{ htmlStr }}</div>
-    <div v-html="htmlStr"></div>
+    <p>
+      점수는 {{ randomNumber }}점
+      <template v-if="randomNumber > 80">
+        으로 매우 우수합니다
+        <span style="color: red">대단해요!</span>
+      </template>
+    </p>
   </div>
 </template>
 
 <script setup lang="ts">
-const htmlStr = ref(`<a href="https://www.naver.com/">네이버<a/>`);
+const randomNumber = computed(() => Math.round(Math.random() * 100));
 </script>
